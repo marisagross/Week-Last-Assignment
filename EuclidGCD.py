d62f3401df549b7b3e67367a38aff5a9829a0d22
marisagross <marisa.gross@myemail.indwes.edu>
2015-12-10 19:55:21 +0000

    changes

--- EuclidGCD.py
+++ EuclidGCD.py
@@ -1,4 +1,20 @@
+# Marisa Gross
+# CIS 125
+# Last week Assignment
+# Collaborated with Dr. Neumann 
+
 def gCd(x, y):
-    if ???# base case here
-        return ???
-    return gCd(?????)
+    print("x = ",x, "y = ",y)
+    if y == 0:
+        return x
+    else:
+        return gCd(y, x%y)   # return gcd
+        
+    
+def main():
+    x = 252       
+    y = 105
+    print("GCD of",x,",",y,"=",gCd(x,y))
+    
+if __name__ == "__main__":
+    main()
